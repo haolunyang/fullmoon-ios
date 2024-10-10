aaa4a488f8c01edb994b8a969d4cd5d1705e6833
haolunyang <harryyhl@icloud.com>
2024-10-09 19:40:46 -0700

    Mesh gradient and Metal shader.

--- fullmoon/ContentView.swift
+++ fullmoon/ContentView.swift
@@ -21,144 +21,208 @@ struct ContentView: View {
     @FocusState var isPromptFocused: Bool
     @State var currentThread: Thread?
     @Namespace var bottomID
+    
+    @State private var counter: Int = 0
+    @State private var origin: CGPoint = .zero
 
     var body: some View {
         NavigationStack {
-            VStack(spacing: 0) {
-                if let currentThread = currentThread {
-                    ScrollViewReader { scrollView in
-                        ScrollView(.vertical) {
-                            VStack(alignment: .leading, spacing: 0) {
-                                ForEach(currentThread.sortedMessages) { message in
-                                    HStack {
-                                        if message.role == .user {
-                                            Spacer()
-                                        }
-                                        
-                                        Text(try! AttributedString(markdown: message.content.trimmingCharacters(in: .whitespacesAndNewlines),
-                                             options: .init(interpretedSyntax: .inlineOnlyPreservingWhitespace)))
-                                            .textSelection(.enabled)
-                                            .if(message.role == .user) { view in
-                                                view
-                                                    .padding(.horizontal, 16)
-                                                    .padding(.vertical, 12)
-                                                    .background(Color(UIColor.secondarySystemBackground))
-                                                    .mask(RoundedRectangle(cornerRadius: 24))
-                                            }
-                                            .padding(message.role == .user ? .leading : .trailing, 48)
-                                        
-                                        if message.role == .assistant {
-                                            Spacer()
-                                        }
-                                    }
-                                    .padding()
-                                }
-                                
-                                if llm.running && !llm.output.isEmpty {
-                                    HStack {
-                                        Text(try! AttributedString(markdown: llm.output.trimmingCharacters(in: .whitespacesAndNewlines) + " 🌕",
-                                             options: .init(interpretedSyntax: .inlineOnlyPreservingWhitespace)))
-                                            .textSelection(.enabled)
-                                            .multilineTextAlignment(.leading)
-                                            .padding(.trailing, 48)
-                                        
-                                            Spacer()
-                                    }
-                                    .padding()
-                                }
-                            }
-                            
-                            Rectangle()
-                                .fill(.clear)
-                                .frame(height: 1)
-                                .id(bottomID)
-                        }
-                        .onChange(of: llm.output) { _, _ in
-                            scrollView.scrollTo(bottomID)
-                        }
+            GeometryReader { geometry in
+                ZStack {
+                    VStack(spacing: 0) {
+                        MeshGradientView()
+                            .ignoresSafeArea()
+                        Rectangle()
+                            .frame(height: 50)
+                            .foregroundStyle(.black)
                     }
-                    .defaultScrollAnchor(.bottom)
-                    .scrollDismissesKeyboard(.interactively)
-                } else {
-                    Spacer()
-                    Image(systemName: appManager.getMoonPhaseIcon())
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(width: 32, height: 32)
-                        .foregroundStyle(.quaternary)
-                    Spacer()
-                }
-                
-                HStack {
-                    Button {
-                        playHaptic()
-                        showModelPicker.toggle()
-                    } label: {
-                        Group {
-                            Image(systemName: "chevron.up")
-                                .resizable()
-                                .aspectRatio(contentMode: .fit)
-                                .frame(width: 16)
-                                .tint(.primary)
+                    .background(.black)
+                    .overlay {
+                        Image("Grain")
+                            .scaleEffect(0.5)
+                            .ignoresSafeArea()
+                            .blendMode(.overlay)
+                            .opacity(0.6)
+                    }
+                    .keyframeAnimator(
+                        initialValue: 0,
+                        trigger: counter
+                    ) { view, elapsedTime in
+                        view.visualEffect { view, _ in
+                            view.layerEffect(
+                                ShaderLibrary.Ripple(
+                                    .float2(origin),
+                                    .float(elapsedTime),
+                                    .float(12), // amplitude
+                                    .float(15), // frequency
+                                    .float(8),  // decay
+                                    .float(1200) // speed
+                                ),
+                                maxSampleOffset: CGSize(width: 12, height: 12),
+                                isEnabled: 0 < elapsedTime && elapsedTime < 3
+                            )
                         }
-                        .frame(width: 48, height: 48)
-                        .background(
-                            Circle()
-                                .fill(Color(UIColor.secondarySystemBackground))
-                        )
+                    } keyframes: { _ in
+                        MoveKeyframe(0)
+                        LinearKeyframe(3, duration: 3)
                     }
                     
-                    HStack(spacing: 0) {
-                        TextField("message", text: $prompt)
-                            .focused($isPromptFocused)
-                            .textFieldStyle(.plain)
-                            .padding(.horizontal)
-                            .if(idiom == .pad || idiom == .mac) { view in
-                                view
-                                    .onSubmit {
-                                        generate()
+                    VStack(spacing: 0) {
+                        if let currentThread = currentThread {
+                            ScrollViewReader { scrollView in
+                                ScrollView(.vertical) {
+                                    VStack(alignment: .leading, spacing: 0) {
+                                        ForEach(currentThread.sortedMessages) { message in
+                                            HStack {
+                                                if message.role == .user {
+                                                    Spacer()
+                                                }
+                                                
+                                                Text(try! AttributedString(markdown: message.content.trimmingCharacters(in: .whitespacesAndNewlines),
+                                                                           options: .init(interpretedSyntax: .inlineOnlyPreservingWhitespace)))
+                                                .textSelection(.enabled)
+                                                .if(message.role == .user) { view in
+                                                    view
+                                                        .padding(.horizontal, 16)
+                                                        .padding(.vertical, 12)
+                                                        .background(
+                                                            .ultraThinMaterial
+                                                        )
+                                                        .mask(RoundedRectangle(cornerRadius: 24, style: .continuous))
+                                                        .overlay {
+                                                            RoundedRectangle(cornerRadius: 24, style: .continuous)
+                                                                .strokeBorder(.white.opacity(0.1), lineWidth: 1.5)
+                                                        }
+                                                }
+                                                .padding(message.role == .user ? .leading : .trailing, 48)
+                                                
+                                                if message.role == .assistant {
+                                                    Spacer()
+                                                }
+                                            }
+                                            .padding()
+                                        }
+                                        
+                                        if llm.running && !llm.output.isEmpty {
+                                            HStack {
+                                                Text(try! AttributedString(markdown: llm.output.trimmingCharacters(in: .whitespacesAndNewlines) + " ◐",
+                                                                           options: .init(interpretedSyntax: .inlineOnlyPreservingWhitespace)))
+                                                .textSelection(.enabled)
+                                                .multilineTextAlignment(.leading)
+                                                .padding(.trailing, 48)
+                                                
+                                                Spacer()
+                                            }
+                                            .padding()
+                                        }
                                     }
-                                    .submitLabel(.send)
+                                    
+                                    Rectangle()
+                                        .fill(.clear)
+                                        .frame(height: 1)
+                                        .id(bottomID)
+                                }
+                                .onChange(of: llm.output) { _, _ in
+                                    scrollView.scrollTo(bottomID)
+                                }
                             }
-                        
-                        Button {
-                            generate()
-                        } label: {
-                            Image(systemName: "arrow.up.circle.fill")
+                            .defaultScrollAnchor(.bottom)
+                            #if !os(visionOS)
+                            .scrollDismissesKeyboard(.interactively)
+                            #endif
+                        } else {
+                            Spacer()
+                            Image(systemName: appManager.getMoonPhaseIcon())
                                 .resizable()
                                 .aspectRatio(contentMode: .fit)
-                                .frame(width: 24, height: 24)
+                                .frame(width: 32, height: 32)
+                                .foregroundStyle(.quaternary)
+                            Spacer()
                         }
-                        .disabled(llm.running || prompt.isEmpty)
-                        .padding(.trailing)
+                        
+                        HStack {
+                            Button {
+                                playHaptic()
+                                showModelPicker.toggle()
+                            } label: {
+                                Group {
+                                    Image(systemName: "chevron.up")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 16)
+                                        .tint(.primary)
+                                }
+                                .frame(width: 48, height: 48)
+                                .background(
+                                    Circle()
+                                        .foregroundStyle(.ultraThinMaterial)
+                                        .overlay {
+                                            Circle()
+                                                .strokeBorder(.white.opacity(0.1), lineWidth: 1.5)
+                                        }
+                                )
+                            }
+                            
+                            HStack(spacing: 0) {
+                                TextField("message", text: $prompt)
+                                    .focused($isPromptFocused)
+                                    .textFieldStyle(.plain)
+                                    .padding(.horizontal)
+                                    .if(idiom == .pad || idiom == .mac) { view in
+                                        view
+                                            .onSubmit {
+                                                generate()
+                                            }
+                                            .submitLabel(.send)
+                                    }
+                                
+                                Button {
+                                    origin = CGPoint(x: geometry.size.width, y: geometry.size.height)
+                                    counter += 1
+                                    generate()
+                                } label: {
+                                    Image(systemName: "arrow.up.circle.fill")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(width: 24, height: 24)
+                                        .offset(x: 2.5) // make the symbol concentric with the text field
+                                }
+                                .disabled(llm.running || prompt.isEmpty)
+                                .padding(.trailing)
+                            }
+                            .frame(height: 48)
+                            .background(
+                                Capsule()
+                                    .foregroundStyle(.ultraThinMaterial)
+                                    .overlay {
+                                        Capsule()
+                                            .strokeBorder(.white.opacity(0.1), lineWidth: 1.5)
+                                    }
+                            )
+                        }
+                        .padding()
                     }
-                    .frame(height: 48)
-                    .background(
-                        Capsule()
-                            .fill(Color(UIColor.secondarySystemBackground))
-                    )
-                }
-                .padding()
-            }
-            .navigationTitle(chatTitle)
-            .toolbarRole(.editor)
-            .navigationBarTitleDisplayMode(.inline)
-            .toolbar {
-                ToolbarItem(placement: .topBarLeading) {
-                    Button(action: {
-                        playHaptic()
-                        showChats.toggle()
-                    }) {
-                        Image(systemName: "list.bullet")
-                    }
-                }
-                
-                ToolbarItem(placement: .topBarTrailing) {
-                    Button(action: {
-                        playHaptic()
-                        showSettings.toggle()
-                    }) {
-                        Image(systemName: "gear")
+                    .navigationTitle(chatTitle)
+                    .toolbarRole(.editor)
+                    .navigationBarTitleDisplayMode(.inline)
+                    .toolbar {
+                        ToolbarItem(placement: .topBarLeading) {
+                            Button(action: {
+                                playHaptic()
+                                showChats.toggle()
+                            }) {
+                                Image(systemName: "list.bullet")
+                            }
+                        }
+                        
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Button(action: {
+                                playHaptic()
+                                showSettings.toggle()
+                            }) {
+                                Image(systemName: "gear")
+                            }
+                        }
                     }
                 }
             }
@@ -229,7 +293,7 @@ struct ContentView: View {
             }
         }
         
-        return "chat"
+        return "fullmoon"
     }
 
     private func generate() {
@@ -264,8 +328,10 @@ struct ContentView: View {
     }
     
     func playHaptic() {
+        #if !os(visionOS)
         let impact = UIImpactFeedbackGenerator(style: .soft)
         impact.impactOccurred()
+        #endif
     }
     
     private func copyToClipboard(_ string: String) {
